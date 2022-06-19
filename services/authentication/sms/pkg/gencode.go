8fe4d19214f3cfca98383fe152b0e0b1018690dd
Celio Grand <celiogrand@outlook.com>
2022-06-20 01:08:21 +0800

    Generate SMS  verification code

---
+++ services/authentication/sms/pkg/gencode.go
@@ -0,0 +1,55 @@
+package sms
+
+import (
+	"crypto/rand"
+	"encoding/binary"
+	"math/big"
+)
+
+var maxCode uint64 = 1000000
+
+func byteToCode1(b []byte) uint64 {
+	return binary.BigEndian.Uint64(b[:])
+}
+
+func byteToCode2(b []byte) uint64 {
+	var code uint64
+	for _, v := range b {
+		code <<= 8
+		code |= uint64(v)
+	}
+	return code
+}
+
+func genCode1() (uint64, error) {
+	b := make([]byte, 8)
+	_, err := rand.Read(b)
+	if err != nil {
+		return 0, err
+	}
+	code := byteToCode1(b)
+	code %= maxCode
+	return code, nil
+}
+
+func genCode2() (uint64, error) {
+	b := make([]byte, 8)
+	_, err := rand.Read(b)
+	if err != nil {
+		return 0, err
+	}
+	code := byteToCode2(b)
+	code %= maxCode
+	return code, nil
+}
+
+func genCode3() (uint64, error) {
+	codeBig, err := rand.Int(rand.Reader, big.NewInt(int64(maxCode)))
+	if err != nil {
+		return 0, err
+	}
+	code := codeBig.Uint64()
+	return code, nil
+}
+
+var GenCode func() (uint64, error) = genCode1
---
+++ services/authentication/sms/pkg/gencode_test.go
@@ -0,0 +1,48 @@
+package sms
+
+import (
+	"crypto/rand"
+	"testing"
+)
+
+func TestGenCode(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		got, err := GenCode()
+		if err != nil {
+			t.Errorf("%d: GenCode() error", i)
+		}
+		t.Logf("%d: %06d", i, got)
+		if !(got < maxCode) {
+			t.Errorf("%d: GenCode() = %d; want in range [0, maxCode)", i, got)
+		}
+	}
+}
+
+func TestByteToCode(t *testing.T) {
+	b := make([]byte, 8)
+	_, err := rand.Read(b)
+	if err != nil {
+		t.Errorf("crypto/rand error")
+	}
+	if byteToCode1(b) != byteToCode2(b) {
+		t.Errorf("ByteToCode functions unmatch")
+	}
+}
+
+func BenchmarkGenCode1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		genCode1()
+	}
+}
+
+func BenchmarkGenCode2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		genCode2()
+	}
+}
+
+func BenchmarkGenCode3(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		genCode3()
+	}
+}
