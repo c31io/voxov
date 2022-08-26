cdb7ed961a8d44a6967c16aaa2b34eb5fb28ff15
Celio Grand <celiogrand@outlook.com>
2022-08-26 21:06:07 +0800

    gate listen

--- pkg/gate/server.go
+++ pkg/gate/server.go
@@ -4,10 +4,6 @@ import (
 	pb "github.com/c31io/voxov/api"
 )
 
-var (
-	_ = "env"
-)
-
 type Server struct {
 	pb.UnimplementedVOxOVServer
 }
--- service/gate/gate.go
+++ service/gate/gate.go
@@ -1,13 +1,36 @@
 package main
 
 import (
-	"context"
+	"log"
+	"net"
+	"os"
 
 	"github.com/c31io/voxov/pkg/gate"
+	"google.golang.org/grpc"
+
+	pb "github.com/c31io/voxov/api"
 )
 
+var gatePort string
+
+func init() {
+	val, ok := os.LookupEnv("GATE_PORT")
+	if !ok {
+		gatePort = "10001"
+	} else {
+		gatePort = val
+	}
+}
+
 func main() {
-	_ = context.Background()
-	_ = gate.Rdb
-	_ = new(gate.Session)
+	lis, err := net.Listen("tcp", gatePort)
+	if err != nil {
+		log.Fatalf("gate failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterVOxOVServer(s, &gate.Server{})
+	log.Printf("gate server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("gate failed to serve: %v", err)
+	}
 }
