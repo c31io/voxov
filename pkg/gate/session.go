4a63bed5a31f127db98d3b62b12e1d140d6d59d8
Celio Grand <celiogrand@outlook.com>
2022-08-29 07:03:07 +0800

    no recover no panic

--- pkg/gate/session.go
+++ pkg/gate/session.go
@@ -3,6 +3,7 @@ package gate
 import (
 	"crypto/rand"
 	"errors"
+	"log"
 )
 
 // It takes more than 1.7*10^8 years to randomly guess one
@@ -20,7 +21,7 @@ func genToken() []byte {
 	b := make([]byte, tokenBytes)
 	_, err := rand.Read(b)
 	if err != nil {
-		panic("session: crypto/rand returned an error")
+		log.Fatal("session: crypto/rand returned an error")
 	}
 	return b
 }
