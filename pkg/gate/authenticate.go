5ffcf8c53e676afa58064d0d400025029a999215
Celio Grand <celiogrand@outlook.com>
2022-09-27 00:41:08 +0800

    WhoAmI & Health

--- pkg/gate/authenticate.go
+++ pkg/gate/authenticate.go
@@ -29,18 +29,39 @@ func (s *Server) Authenticate(ctx context.Context, in *pb.AuthenticateRequest) (
 	}
 	conn, err := grpc.Dial(authAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
-		log.Fatalf("Did not connect: %v", err)
+		log.Printf("Did not connect: %v", err)
+		Health.NowDead()
+		return &pb.AuthenticateReply{}, nil
 	}
 	defer conn.Close()
 	c := pbAuth.NewAuthClient(conn)
 	r, err := c.WaitMsg(ctx, &pbAuth.WaitMsgRequest{Token: in.GetToken()})
 	if err != nil {
-		log.Fatal("Failed to get WaitMsgReply")
+		log.Println("Failed to get WaitMsgReply")
+		Health.NowDead()
+		return &pb.AuthenticateReply{}, nil
 	}
 	return &pb.AuthenticateReply{Tel: r.GetTel(), Msg: r.GetMsg()}, nil
 }
 
 // Client checks if sms was received, and if so update session person.
 func (s *Server) WhoAmI(ctx context.Context, in *pb.WhoAmIRequest) (*pb.WhoAmIReply, error) {
-	return &pb.WhoAmIReply{}, nil
+	if !isValidToken(ctx, in.GetToken()) {
+		return &pb.WhoAmIReply{}, nil
+	}
+	conn, err := grpc.Dial(authAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Println("Failed to get WaitMsgReply")
+		Health.NowDead()
+		return &pb.WhoAmIReply{}, nil
+	}
+	defer conn.Close()
+	c := pbAuth.NewAuthClient(conn)
+	r, err := c.CheckMsg(ctx, &pbAuth.CheckMsgRequest{Token: in.GetToken(), Tel: in.GetTel(), Msg: in.GetMsg()})
+	if err != nil {
+		log.Println("Failed to get WhoAmIReply")
+		Health.NowDead()
+		return &pb.WhoAmIReply{}, nil
+	}
+	return &pb.WhoAmIReply{Person: r.GetPerson()}, nil
 }
